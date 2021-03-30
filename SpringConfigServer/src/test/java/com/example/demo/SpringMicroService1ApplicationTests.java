ad5a52b842835ef1f8a86f5d50427bf0c588fdeb
jatinder singh <31757022+baljatinder09@users.noreply.github.com>
2021-03-30 18:03:12 +0530

    Add files via upload

---
+++ SpringConfigServer/src/test/java/com/example/demo/SpringMicroService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
